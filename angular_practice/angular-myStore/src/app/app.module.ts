9048d7d10986b13adf2b97cf8be90acac7f2b73c
blackswan <67410919+blackswan1989@users.noreply.github.com>
2020-12-22 15:27:10 +0900

    Angular Practice file

---
+++ angular_practice/angular-myStore/src/app/app.module.ts
@@ -0,0 +1,48 @@
+// 이 파일에는 앱 전역에 필요한 심볼과 기능이 등록됩니다.
+
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { RouterModule } from "@angular/router";
+import { ReactiveFormsModule } from "@angular/forms";
+
+import { AppComponent } from "./app.component";
+import { TopBarComponent } from "./top-bar/top-bar.component";
+import { ProductListComponent } from "./product-list/product-list.component";
+import { ProductAlertsComponent } from "./product-alerts/product-alerts.component";
+import { ProductDetailsComponent } from "./product-details/product-details.component";
+import { CartService } from "./cart.service";
+import { CartComponent } from "./cart/cart.component";
+import { HttpClientModule } from "@angular/common/http";
+import { ShippingComponent } from "./shipping/shipping.component";
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot([
+      { path: "", component: ProductListComponent },
+      { path: "products/:productId", component: ProductDetailsComponent },
+      { path: "cart", component: CartComponent }, // */cart로 연결
+      { path: "shipping", component: ShippingComponent } // */shipping로 연결
+    ])
+  ],
+  declarations: [
+    AppComponent,
+    TopBarComponent,
+    ProductListComponent,
+    ProductAlertsComponent,
+    ProductDetailsComponent,
+    CartComponent,
+    ShippingComponent
+  ],
+  bootstrap: [AppComponent],
+  providers: [CartService]
+})
+export class AppModule {}
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
