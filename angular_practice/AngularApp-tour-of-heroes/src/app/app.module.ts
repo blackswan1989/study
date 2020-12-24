5d4231037f2df9afc530b816da090bd906bde572
blackswan <67410919+blackswan1989@users.noreply.github.com>
2020-12-24 18:10:02 +0900

    heroAPP : app.component

--- angular_practice/AngularApp-tour-of-heroes/src/app/app.module.ts
+++ angular_practice/AngularApp-tour-of-heroes/src/app/app.module.ts
@@ -2,15 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'; // <-- NgModel은 이 패키지가 제공합니다.
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { MessagesComponent } from './messages/messages.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+import { AppRoutingModule } from './app-routing.module';
 
 //애플리케이션 동작에 필요한 메타데이터는 @NgModule 데코레이터에 지정해준다.
 @NgModule({
-  declarations: [AppComponent, HeroesComponent, HeroDetailComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
+  declarations: [
+    AppComponent,
+    HeroesComponent,
+    HeroDetailComponent,
+    MessagesComponent,
+    DashboardComponent,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- angular_practice/AngularApp-tour-of-heroes/src/app/mock-heroes.ts
+++ angular_practice/AngularApp-tour-of-heroes/src/app/mock-heroes.ts
@@ -1,12 +1,22 @@
 import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
+  { id: 1, name: 'Iron Man' },
+  { id: 2, name: 'Black Widow' },
+  { id: 3, name: 'Hulk' },
+  { id: 4, name: 'Thor' },
+  { id: 5, name: 'Spider Man' },
+  { id: 6, name: 'Wonder Woman' },
+  { id: 7, name: 'Super Man' },
+  { id: 8, name: 'Captain America' },
+  { id: 9, name: 'Black Panther' },
+  { id: 10, name: 'Ant Man' },
   { id: 11, name: 'Dr Nice' },
   { id: 12, name: 'Narco' },
   { id: 13, name: 'Bombasto' },
   { id: 14, name: 'Celeritas' },
   { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
+  { id: 16, name: 'Rubber Man' },
   { id: 17, name: 'Dynama' },
   { id: 18, name: 'Dr IQ' },
   { id: 19, name: 'Magma' },
