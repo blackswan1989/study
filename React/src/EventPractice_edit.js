8c19a420783eaf699d8be7662cad832806fa1467
HyoJin.Nam <67410919+blackswan1989@users.noreply.github.com>
2021-06-12 00:41:54 +0900

    add handleKeyPress

--- React/src/EventPractice_edit.js
+++ React/src/EventPractice_edit.js
@@ -23,12 +23,19 @@ class EventPracticeEdit extends Component {
     })
   }
 
+  // 키를 눌렀을 때 발생하는 KeyPress 이벤트. comment 인풋에서 enter를 눌렀을때 `handleClick` 메서드가 호출되도록 함
+  handleKeyPress = (e) => {
+    if(e.key === 'Enter') {
+      this.handleClick();
+    }
+  }
+
   render() {
     return (
       <div>
         <h1>Event Practice "[e.target.name]"</h1>
         <input type="text" name="username" placeholder="User Name" value={this.state.username} onChange={this.handleChange}/>
-        <input type="text" name="message" placeholder="Text Enter" value={this.state.message} onChange={this.handleChange}/>
+        <input type="text" name="message" placeholder="Text Enter" value={this.state.message} onChange={this.handleChange} onKeyPress={this.handleKeyPress}/>
         <button onClick={this.handleClick}> 확인 </button>
       </div>
     )
